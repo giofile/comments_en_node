0794bf34abfa245e87e3badc77cb8704e547c8cd
giofile <77681874+giofile@users.noreply.github.com>
2021-07-21 13:08:19 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("Hallo Winc Academy studenten");
+// Dit is een grote som
+/* let sumOne = 1230941
+let sumTwo = 1823794
+console.log(sumOne * sumTwo);
+let sumThree = 637263
+let sumFour = 54
+console.log(sumThree/ sumFour); */
